36b8a84adda7e51e11ad2607d4e1fdbc52299dca
Siddharth <siddharth@infostride.com>
2020-02-19 02:32:38 +0530

    content area added

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,10 +1,7 @@
 import React from "react";
 import "./Header.css";
-import ReactDOM from "react-dom";
 import { Button, Modal } from "react-bootstrap";
-import { Dropdown, MenuItem, DropdownButton } from "react-bootstrap";
-import axios from 'axios';
-
+import swal from 'sweetalert';
 
 class Header extends React.Component {
     constructor() {
@@ -12,9 +9,9 @@ class Header extends React.Component {
         this.state = {
             show: false,
             setShow: false,
-            task:"",
-            category:"",
-            dueDate:''
+            task: "",
+            category: "",
+            dueDate: " "
         }
     }
     handleShow = () => {
@@ -29,60 +26,90 @@ class Header extends React.Component {
             setShow: false
         })
     }
-changeHandler=(e)=>{
-    this.setState({[e.target.name]:e.target.value})
-}
-submitHandler=e=>{
-    e.preventDefault();
-    console.log(this.state);
-    axios.post("http://localhost:4070/app/task",this.state)
-    .then(response=>{
-        console.log(response);
-    })
-    .catch(err=>{
-        console.log(err);
-    })
-   
-}
+    changeHandler = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+    submitHandler = e => {
+        e.preventDefault();
+        console.log(this.state);
+        swal("Task Added Successfully !");
+        this.setState({
+            task:'',
+            dueDate:'',
+            category:''
+        })
+        // axios.post("http://localhost:4070/app/task",this.state)
+        // .then(response=>{
+        //     console.log(response);
+        // })
+        // .catch(err=>{
+        //     console.log(err);
+        // })
+
+    }
 
     render() {
-        const { show,task,category,dueDate } = this.state;
+        const { show, task, category, dueDate } = this.state;
         return (
             <>
                 <div className="header">
-                    Task Manager
+                <span style={{ fontFamily: "Times New Roman", marginLeft :"170px"}}>Task Manager</span>
           <Button type="button" className="addtask" onClick={this.handleShow} >+ Add Task</Button>
                     <Modal show={show} onHide={this.handleClose}>
                         <Modal.Header closeButton>
                             <Modal.Title>Add Title</Modal.Title>
                         </Modal.Header>
                         <form onSubmit={this.submitHandler}>
-                        <Modal.Body>Task <input class="form-control" type="text" placeholder="Enter the Task" name="task" value={task} onChange={this.changeHandler} ></input></Modal.Body>
-                        <Modal.Body>Due Date  <input class="form-control" type="datetime-local" id="birthdaytime" name="dueDate" value={dueDate} onChange={this.changeHandler}></input></Modal.Body>
-                        <Modal.Body>Category<select id="inputState" class="form-control" name="category" value={category} onChange={this.changeHandler}>
-                            <option selected>Choose...</option>
-                            <option>Urgent</option>
-                            <option>Important</option>
-                            <option>Others</option>
-                        </select> </Modal.Body>
-                        <Modal.Footer>
-                            <Button variant="secondary" onClick={this.handleClose}>
-                                Close
+                            <Modal.Body>Task <input class="form-control" type="text" placeholder="Enter the Task" name="task" value={task} onChange={this.changeHandler} ></input></Modal.Body>
+                            <Modal.Body>Due Date  <input class="form-control" type="date" name="dueDate" value={dueDate} onChange={this.changeHandler}></input></Modal.Body>
+                            <Modal.Body>Category<select id="inputState" class="form-control" name="category" value={category} onChange={this.changeHandler}>
+                                <option selected>Choose...</option>
+                                <option>Urgent</option>
+                                <option>Important</option>
+                                <option>Others</option>
+                            </select> </Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="secondary" onClick={this.handleClose}>
+                                    Close
                             </Button>
-                            <Button variant="primary" onClick={this.handleClose} type="submit">
-                                Submit
+                                <Button variant="primary" onClick={this.handleClose} type="submit">
+                                    Submit
                             </Button>
-                        </Modal.Footer>
+                            </Modal.Footer>
                         </form>
-                        {/* <Modal.Footer>
-                            <Button variant="secondary" onClick={this.handleClose}>
-                                Close
-                            </Button>
-                            <Button variant="primary" onClick={this.handleClose} type="submit">
-                                Submit
-                            </Button>
-                        </Modal.Footer> */}
                     </Modal>
+                    <div className="container-fluid back">
+                        <div className="row">
+                            <div className="col-md-4 first">
+                                <h3>Urgent</h3>
+                                <div className="card">
+                                    <div className="card-body">
+                                   
+                                    <h5 className="card-title">Task To be Done</h5>
+                                        <h6 className="card-subtitle mb-2 text-muted">Due Date</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 second">
+                                <h3>Important</h3>
+                                <div className="card">
+                                    <div className="card-body">
+                                        <h5 className="card-title">Task To be Done</h5>
+                                        <h6 className="card-subtitle mb-2 text-muted">Due Date</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 third">
+                                <h3>Others</h3>
+                                <div className="card" >
+                                    <div className="card-body">
+                                        <h5 className="card-title">Task To be Done</h5>
+                                        <h6 className="card-subtitle mb-2 text-muted">Due Date</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </>
         );
