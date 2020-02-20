7d3c4726085e54398929f0110a740c82b1740868
kamlesh <kamleshbishnoi812@gmail.com>
2020-02-21 04:30:07 +0530

    console error resolved and added functionality

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -24,11 +24,9 @@ class Header extends React.Component {
     axios
       .get("http://localhost:4070/app/task")
       .then(response => {
-        // console.log("response.data.data", response.data.data);
         this.setState({
           detail: response.data.data
         });
-        // console.log("response from get api", response);
       })
       .catch(err => {
         console.log(err);
@@ -37,14 +35,10 @@ class Header extends React.Component {
   updatetask(_id, category) {
     axios
       .post("http://localhost:4070/app/task/update", { _id, category })
-      .then(response => {
-        // console.log("response.data.data", response.data.data);
-        // console.log("response from get api", response);
-      })
+      .then(response => {})
       .catch(err => {
         console.log(err);
       });
-    // window.location.reload(false);
   }
   handleShow = () => {
     this.setState({
@@ -67,16 +61,14 @@ class Header extends React.Component {
     this.getdetail();
     this.updatetask();
     e.preventDefault();
-    console.log(this.state);
+
     axios
       .post("http://localhost:4070/app/task", this.state)
       .then(response => {
-        console.log(response);
         this.setState({
           detail: [...this.state.detail, response.data]
         });
         this.handleClose(false);
-        console.log("detatatttatatilsss", this.state.detail);
       })
       .catch(err => {
         console.log(err);
@@ -87,20 +79,13 @@ class Header extends React.Component {
       category: ""
     });
   };
-  // changeHandler = e => {
-  //   this.setState({ [e.target.name]: e.target.value });
-  // };
 
   drag = (e, item) => {
-    console.log("draggg", e, item);
     this.setState({
       drop: item
     });
   };
-  noAllowDrop = e => {
-    console.log("noAllowDrop", e);
-    // e.stopPropagation();
-  };
+  noAllowDrop = e => {};
   drop = (e, category) => {
     this.state.detail.forEach(item => {
       if (item._id === this.state.drop._id) {
@@ -112,10 +97,8 @@ class Header extends React.Component {
       detail: this.state.detail
     });
     this.updatetask(this.state.drop._id, category);
-    // console.log("detail", this.state.detail);
   };
   allowdrop = e => {
-    // console.log("AllowDrop", e);
     e.preventDefault();
   };
 
@@ -131,9 +114,8 @@ class Header extends React.Component {
         axios
           .delete("http://localhost:4070/app/task/delete", { data: { _id } })
           .then(response => {
-            console.log("delete:", response);
             this.setState({
-              detail: this.state.detail.filter(item => item._id != _id)
+              detail: this.state.detail.filter(item => item._id !== _id)
             });
             swal("Task Removed Successfully!", {
               icon: "success"
@@ -148,83 +130,86 @@ class Header extends React.Component {
     });
   };
   render() {
-    console.log("detatatat", this.state.detail);
     const { show, task, category, dueDate, detail } = this.state;
     return (
       <>
-        <div className="header">
-          <span style={{ fontSize: "60px" }}>
-            <b> Task Manager</b>
-          </span>
-        </div>
-        <Modal show={show} onHide={this.handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Add Title</Modal.Title>
-          </Modal.Header>
-          <form onSubmit={this.submitHandler}>
-            <Modal.Body>
-              Task{" "}
-              <input
-                class="form-control"
-                type="text"
-                placeholder="Enter the Task"
-                name="task"
-                value={task}
-                onChange={this.changeHandler}
-              ></input>
-            </Modal.Body>
-            <Modal.Body>
-              Due Date{" "}
-              <input
-                class="form-control"
-                type="date"
-                name="dueDate"
-                value={dueDate}
-                onChange={this.changeHandler}
-              ></input>
-            </Modal.Body>
-            <Modal.Body>
-              Category
-              <select
-                id="inputState"
-                class="form-control"
-                name="category"
-                value={category}
-                onChange={this.changeHandler}
-              >
-                <option selected>Choose...</option>
-                <option>Urgent</option>
-                <option>Important</option>
-                <option>Others</option>
-              </select>{" "}
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={this.handleClose}>
-                Close
-              </Button>
-              <Button variant="primary" type="submit">
-                Add
-              </Button>
-            </Modal.Footer>
-          </form>
-        </Modal>
-        <div className="container-fluid back">
-          <div class="row">
-            <div className="col-md-12">
-            <span class="badge badge-pill badge-light">Categories</span>
+        <div className="container-fluid">
+          <div className="row header">
+            <div className="col-md-12 ">
+              <span style={{ fontSize: "60px" }}>
+                <b> Task Manager</b>
+              </span>
             </div>
           </div>
-          <hr />
-          <div className="container-fluid">
-          <div className="row">
+          <Modal show={show} onHide={this.handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Add Title</Modal.Title>
+            </Modal.Header>
+            <form onSubmit={this.submitHandler}>
+              <Modal.Body>
+                Task{" "}
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="Enter the Task"
+                  name="task"
+                  value={task}
+                  onChange={this.changeHandler}
+                  required
+                ></input>
+              </Modal.Body>
+              <Modal.Body>
+                Due Date{" "}
+                <input
+                  className="form-control"
+                  type="date"
+                  name="dueDate"
+                  value={dueDate}
+                  onChange={this.changeHandler}
+                  required
+                ></input>
+              </Modal.Body>
+              <Modal.Body>
+                Category
+                <select
+                  id="inputState"
+                  className="form-control"
+                  name="category"
+                  value={category}
+                  onChange={this.changeHandler}
+                  required
+                >
+                  <option value>Choose...</option>
+                  <option>Urgent</option>
+                  <option>Important</option>
+                  <option>Others</option>
+                </select>{" "}
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={this.handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" type="submit">
+                  Add
+                </Button>
+              </Modal.Footer>
+            </form>
+          </Modal>
+
+          <div className="row Categories">
+            <div className="col-md-12">
+              <span className="badge badge-pill badge-light">Categories</span>
+            </div>
+          </div>
+          <div className="row back">
             <div
               className="col-md-4 first"
               id="Urgent"
               onDrop={e => this.drop(e, "Urgent")}
               onDragOver={this.allowdrop}
             >
-            
               <h3>Urgent</h3>
+
               {detail
                 .filter(item => item.category === "Urgent")
                 .map(item => (
@@ -236,10 +221,10 @@ class Header extends React.Component {
                     onDragOver={this.noAllowDrop}
                   >
                     <i
-                      class="fa fa-trash-o"
+                      className="fa fa-trash-o"
                       onClick={() => this.taskDelete(item._id)}
                     ></i>
-                   
+
                     <div className="card-body" style={{ display: "contents" }}>
                       <h5 className="card-title">Task :&nbsp;{item.task}</h5>
                       <h6 className="card-subtitle mb-2 text-muted">
@@ -247,8 +232,8 @@ class Header extends React.Component {
                       </h6>
                     </div>
                     <span className="createdate">
-                      &nbsp;<i class="fa fa-clock-o"></i> &nbsp;Task Added on :
-                      {item.creationDate}{" "}
+                      &nbsp;<i className="fa fa-clock-o"></i> &nbsp;Task Added
+                      on :{item.creationDate}{" "}
                     </span>
                   </div>
                 ))}
@@ -259,7 +244,6 @@ class Header extends React.Component {
               onDrop={e => this.drop(e, "Important")}
               onDragOver={this.allowdrop}
             >
-              
               <h3>Important</h3>
               {detail
                 .filter(item => item.category === "Important")
@@ -272,7 +256,7 @@ class Header extends React.Component {
                     onDragOver={this.noAllowDrop}
                   >
                     <i
-                      class="fa fa-trash-o delete"
+                      className="fa fa-trash-o delete"
                       onClick={() => this.taskDelete(item._id)}
                     ></i>
                     <div className="card-body" style={{ display: "contents" }}>
@@ -281,7 +265,7 @@ class Header extends React.Component {
                         Due Date :&nbsp; {item.dueDate}
                       </h6>
                     </div>
-                    <i class="far fa-clock"></i>
+                    <i className="far fa-clock"></i>
                     <span className="createdate">
                       &nbsp;<i className="fa fa-clock-o"></i> &nbsp;Task Added
                       on :{item.creationDate}{" "}
@@ -290,12 +274,11 @@ class Header extends React.Component {
                 ))}
             </div>
             <div
-              className="col-md-4 first"
+              className="col-md-4 first "
               id="Others"
               onDrop={e => this.drop(e, "Others")}
               onDragOver={this.allowdrop}
             >
-             
               <h3>Others</h3>
               {detail
                 .filter(item => item.category === "Others")
@@ -308,7 +291,7 @@ class Header extends React.Component {
                     onDragOver={this.noAllowDrop}
                   >
                     <i
-                      class="fa fa-trash-o"
+                      className="fa fa-trash-o"
                       onClick={() => this.taskDelete(item._id)}
                     ></i>
                     <div className="card-body" style={{ display: "contents" }}>
@@ -318,8 +301,8 @@ class Header extends React.Component {
                       </h6>
                     </div>
                     <span className="createdate">
-                      &nbsp;<i class="fa fa-clock-o"></i> &nbsp;Task Added on :
-                      {item.creationDate}{" "}
+                      &nbsp;<i className="fa fa-clock-o"></i> &nbsp;Task Added
+                      on :{item.creationDate}{" "}
                     </span>
                   </div>
                 ))}
@@ -331,9 +314,8 @@ class Header extends React.Component {
               className="addtask mybutton"
               onClick={this.handleShow}
             >
-              <i class="fa fa-plus" aria-hidden="true"></i>&nbsp;Add Task
+              <i className="fa fa-plus" aria-hidden="true"></i>&nbsp;Add Task
             </Button>
-          </div>
           </div>
         </div>
       </>
