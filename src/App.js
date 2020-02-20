a076366b971e946fca1f1adbfc72e7f774b06ee6
Purushottam Kumar <puru.thegamer789@gmail.com>
2020-02-20 23:06:58 +0530

    functionality added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Button, Modal } from "react-bootstrap";
 
 import Header from "./Header/Header";
 
 function App() {
+
   return (
     <div className="App">
-     <Header />
-    </div>
+      <Header />
+    </div >
   );
 }
 
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -147,183 +147,190 @@ class Header extends React.Component {
             icon: "warning",
             buttons: true,
             dangerMode: true,
-          })
-          .then((willDelete) => {
-            if (willDelete) {
-              swal("Task Removed Successfully!", {
-                icon: "success",
-              });
-            } else {
-              swal("Your task is safe!");
-            }
-          });
+        })
+            .then((willDelete) => {
+                if (willDelete) {
+                    swal("Task Removed Successfully!", {
+                        icon: "success",
+                    });
+                } else {
+                    swal("Your task is safe!");
+                }
+            });
     }
     render() {
         console.log("detatatat", this.state.detail);
         const { show, task, category, dueDate, detail } = this.state;
         return (
             <>
+                
+                    {/* <Button className="addtask" onClick={this.handleShow} id="mybutton">
+                        &nbsp;&nbsp;Add Task</Button> */}
+             
                 <div className="header">
-                    <span style={{ fontFamily: "Times New Roman", marginLeft: "170px" }}>
+
+                    <span style={{ fontFamily: "Times New Roman", fontSize: "60px" }}>
                         Task Manager
-          </span>
-                    <Button type="button" className="addtask" onClick={this.handleShow}>
+                                </span>
+                    <Button type="button" className="addtask mybutton" onClick={this.handleShow} >
                         + Add Task
           </Button>
-                    <Modal show={show} onHide={this.handleClose}>
-                        <Modal.Header closeButton>
-                            <Modal.Title>Add Title</Modal.Title>
-                        </Modal.Header>
-                        <form onSubmit={this.submitHandler}>
-                            <Modal.Body>
-                                Task{" "}
-                                <input
-                                    class="form-control"
-                                    type="text"
-                                    placeholder="Enter the Task"
-                                    name="task"
-                                    value={task}
-                                    onChange={this.changeHandler}
-                                ></input>
-                            </Modal.Body>
-                            <Modal.Body>
-                                Due Date{" "}
-                                <input
-                                    class="form-control"
-                                    type="date"
-                                    name="dueDate"
-                                    value={dueDate}
-                                    onChange={this.changeHandler}
-                                ></input>
-                            </Modal.Body>
-                            <Modal.Body>
-                                Category
+                </div>
+
+                <Modal show={show} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Add Title</Modal.Title>
+                    </Modal.Header>
+                    <form onSubmit={this.submitHandler}>
+                        <Modal.Body>
+                            Task{" "}
+                            <input
+                                class="form-control"
+                                type="text"
+                                placeholder="Enter the Task"
+                                name="task"
+                                value={task}
+                                onChange={this.changeHandler}
+                            ></input>
+                        </Modal.Body>
+                        <Modal.Body>
+                            Due Date{" "}
+                            <input
+                                class="form-control"
+                                type="date"
+                                name="dueDate"
+                                value={dueDate}
+                                onChange={this.changeHandler}
+                            ></input>
+                        </Modal.Body>
+                        <Modal.Body>
+                            Category
                 <select
-                                    id="inputState"
-                                    class="form-control"
-                                    name="category"
-                                    value={category}
-                                    onChange={this.changeHandler}
-                                >
-                                    <option selected>Choose...</option>
-                                    <option>Urgent</option>
-                                    <option>Important</option>
-                                    <option>Others</option>
-                                </select>{" "}
-                            </Modal.Body>
-                            <Modal.Footer>
-                                <Button variant="secondary" onClick={this.handleClose}>
-                                    Close
+                                id="inputState"
+                                class="form-control"
+                                name="category"
+                                value={category}
+                                onChange={this.changeHandler}
+                            >
+                                <option selected>Choose...</option>
+                                <option>Urgent</option>
+                                <option>Important</option>
+                                <option>Others</option>
+                            </select>{" "}
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={this.handleClose}>
+                                Close
                 </Button>
-                                <Button variant="primary" type="submit">
-                                    Add
+                            <Button variant="primary" type="submit">
+                                Add
                 </Button>
-                            </Modal.Footer>
-                        </form>
-                    </Modal>
-                    <div className="container-fluid back">
-                        <span className="badge badge-pill badge-light">Categories</span>
-                        <div className="row">
-                            <div
-                                className="col-md-4 first"
-                                id="Urgent"
-                                onDrop={e => this.drop(e, "Urgent")}
-                                onDragOver={this.allowdrop}
-                            >
-                                {this.props.children}
-                                <h3>Urgent</h3>
-                                {detail
-                                    .filter(item => item.category === "Urgent")
-                                    .map(item => (
-                                        <div
-                                            className="card"
-                                            id={item._id}
-                                            draggable="true"
-                                            onDragStart={e => this.drag(e, item)}
-                                            onDragOver={this.noAllowDrop}
-                                        >
-                                            <button type="button" onClick={this.taskDelete} class="btn btn-danger remove"><i class="fa fa-trash-o"></i>&nbsp;Remove</button>
-                                            {this.props.children}
-                                            <div className="card-body">
-                                            
-                                                <h5 className="card-title">{item.task}</h5>
-                                                <h6 className="card-subtitle mb-2 text-muted">
-                                                    {item.dueDate}
-                                                </h6>
-                                            </div>
-                                            <span className="createdate">
-                                                &nbsp;<i class="fa fa-clock-o"></i> &nbsp;Task Added on
-                        :{item.creationDate}{" "}
-                                            </span>
-                                        </div>
-                                    ))}
-                            </div>
-                            <div
-                                className="col-md-4 first"
-                                id="Important"
-                                onDrop={e => this.drop(e, "Important")}
-                                onDragOver={this.allowdrop}
-                            >
-                                {this.props.children}
-                                <h3>Important</h3>
-                                {detail
-                                    .filter(item => item.category === "Important")
-                                    .map(item => (
-                                        <div
-                                            className="card"
-                                            id={item._id}
-                                            draggable="true"
-                                            onDragStart={e => this.drag(e, item)}
-                                            onDragOver={this.noAllowDrop}
-                                        >
+                        </Modal.Footer>
+                    </form>
+                </Modal>
+                <div className="container-fluid back">
+                    <span className="badge badge-pill badge-light">Categories</span>
+                    <div className="row">
+                        <div
+                            className="col-md-4 first"
+                            id="Urgent"
+                            onDrop={e => this.drop(e, "Urgent")}
+                            onDragOver={this.allowdrop}
+                        >
+                            {this.props.children}
+                            <h3>Urgent</h3>
+                            {detail
+                                .filter(item => item.category === "Urgent")
+                                .map(item => (
+                                    <div
+                                        className="card"
+                                        id={item._id}
+                                        draggable="true"
+                                        onDragStart={e => this.drag(e, item)}
+                                        onDragOver={this.noAllowDrop}
+                                    >
                                         <button type="button" onClick={this.taskDelete} class="btn btn-danger remove"><i class="fa fa-trash-o"></i>&nbsp;Remove</button>
-                                            <div className="card-body">
-                                                <h5 className="card-title">{item.task}</h5>
-                                                <h6 className="card-subtitle mb-2 text-muted">
-                                                    {item.dueDate}
-                                                </h6>
-                                            </div>
-                                            <i class="far fa-clock"></i>
-                                            <span className="createdate">
-                                                &nbsp;<i className="fa fa-clock-o"></i> &nbsp;Task Added on
-                        :{item.creationDate}{" "}
-                                            </span>
+                                        {this.props.children}
+                                        <div className="card-body">
+
+                                            <h5 className="card-title">{item.task}</h5>
+                                            <h6 className="card-subtitle mb-2 text-muted">
+                                                {item.dueDate}
+                                            </h6>
                                         </div>
-                                    ))}
-                            </div>
-                            <div
-                                className="col-md-4 first"
-                                id="Others"
-                                onDrop={e => this.drop(e, "Others")}
-                                onDragOver={this.allowdrop}
-                            >
-                                {this.props.children}
-                                <h3>Others</h3>
-                                {detail
-                                    .filter(item => item.category === "Others")
-                                    .map(item => (
-                                        <div
-                                            className="card"
-                                            id={item._id}
-                                            draggable="true"
-                                            onDragStart={e => this.drag(e, item)}
-                                            onDragOver={this.noAllowDrop}
-                                        >
-                                        <button type="button" onClick={this.taskDelete} class="btn btn-danger remove"><i class="fa fa-trash-o"></i>&nbsp;Remove</button>
-                                            <div className="card-body">
-                                                <h5 className="card-title">{item.task}</h5>
-                                                <h6 className="card-subtitle mb-2 text-muted">
-                                                    {item.dueDate}
-                                                </h6>
-                                            </div>
-                                            <span className="createdate">
-                                                &nbsp;<i class="fa fa-clock-o"></i> &nbsp;Task Added on
+                                        <span className="createdate">
+                                            &nbsp;<i class="fa fa-clock-o"></i> &nbsp;Task Added on
                         :{item.creationDate}{" "}
-                                            </span>
-                                        </div>
-                                    ))}
-                            </div>
+                                        </span>
+                                    </div>
+                                ))}
                         </div>
+                        <div
+                            className="col-md-4 first"
+                            id="Important"
+                            onDrop={e => this.drop(e, "Important")}
+                            onDragOver={this.allowdrop}
+                        >
+                            {this.props.children}
+                            <h3>Important</h3>
+                            {detail
+                                .filter(item => item.category === "Important")
+                                .map(item => (
+                                    <div
+                                        className="card"
+                                        id={item._id}
+                                        draggable="true"
+                                        onDragStart={e => this.drag(e, item)}
+                                        onDragOver={this.noAllowDrop}
+                                    >
+                                        <button type="button" onClick={this.taskDelete} class="btn btn-danger remove"><i class="fa fa-trash-o"></i>&nbsp;Remove</button>
+                                        <div className="card-body">
+                                            <h5 className="card-title">{item.task}</h5>
+                                            <h6 className="card-subtitle mb-2 text-muted">
+                                                {item.dueDate}
+                                            </h6>
+                                        </div>
+                                        <i class="far fa-clock"></i>
+                                        <span className="createdate">
+                                            &nbsp;<i className="fa fa-clock-o"></i> &nbsp;Task Added on
+                        :{item.creationDate}{" "}
+                                        </span>
+                                    </div>
+                                ))}
+                        </div>
+                        <div
+                            className="col-md-4 first"
+                            id="Others"
+                            onDrop={e => this.drop(e, "Others")}
+                            onDragOver={this.allowdrop}
+                        >
+                            {this.props.children}
+                            <h3>Others</h3>
+                            {detail
+                                .filter(item => item.category === "Others")
+                                .map(item => (
+                                    <div
+                                        className="card"
+                                        id={item._id}
+                                        draggable="true"
+                                        onDragStart={e => this.drag(e, item)}
+                                        onDragOver={this.noAllowDrop}
+                                    >
+                                        <button type="button" onClick={this.taskDelete} class="btn btn-danger remove"><i class="fa fa-trash-o"></i>&nbsp;Remove</button>
+                                        <div className="card-body">
+                                            <h5 className="card-title">{item.task}</h5>
+                                            <h6 className="card-subtitle mb-2 text-muted">
+                                                {item.dueDate}
+                                            </h6>
+                                        </div>
+                                        <span className="createdate">
+                                            &nbsp;<i class="fa fa-clock-o"></i> &nbsp;Task Added on
+                        :{item.creationDate}{" "}
+                                        </span>
+                                    </div>
+                                ))}
+                        </div>
+
                         <hr></hr>
                     </div>
                 </div>
